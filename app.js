7cf74d0a59d25bc3724c53b9336921a86f49f019
kevinbundo <kevin_bundo@yahoo.com>
2020-12-23 15:48:30 +0100

    Deploy

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ app.post("/", function(req, res){
 
 })
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log("Server is running on port 3000");
 });
 
